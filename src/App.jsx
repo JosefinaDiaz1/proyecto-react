6f97e24301b063d30cbb78c844bba2be23cb2b64
josefinadiaz <josefinamadiaz@gmail.com>
2022-06-27 23:31:10 -0300

    desafio contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import {NavBar} from "./components/NavBar";
 import {ItemListContainer} from "./ItemListContainer"
 
+
 function App() {
   return <>
           <NavBar/>
--- src/ItemListContainer/index.jsx
+++ src/ItemListContainer/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Tittle from "../Tittle";
+import ItemCount from "./ItemsCount"
 
 export const ItemListContainer = (props) =>{
+
+        const onAdd=(quantity)=>{
+            console.log("Compraste ${quantity} unidades")
+
+        }
         return(
-            <Tittle greeting={props.texto} />
+            <>
+                <Tittle greeting={props.texto} />
+                <ItemCount initial={1} stock={15} onAdd={onAdd}/>
+            </>
         );
 }
 
